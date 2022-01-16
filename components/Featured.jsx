072a149059983fdf328c7bbedfe1a34c674fa315
adedayo888 <dayoadeniji69@gmail.com>
2022-01-16 18:43:34 +0000

    refactor code

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,7 +1,82 @@
-import React from 'react';
+import styles from '../styles/Featured.module.css';
+import Image from 'next/image';
+import { useState } from 'react';
 
 const Featured = () => {
-  return <div>Featured</div>;
+  const [index, setIndex] = useState(0);
+  const images = ['/img/pizza1.jpg', '/img/pizza2.jpg', '/img/pizza3.jpg'];
+
+  const handleArrow = (direction) => {
+    if (direction === 'l') {
+      setIndex(index !== 0 ? index - 1 : 2);
+    }
+    if (direction === 'r') {
+      setIndex(index !== 2 ? index + 1 : 0);
+    }
+  };
+
+  return (
+    <>
+      <div
+        style={{
+          display: 'flex',
+        }}
+        className={styles.title}>
+        <h3
+          style={{
+            fontSize: '18px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginBottom: '15px',
+          }}>
+          Featured Products
+        </h3>
+      </div>
+      <div className={styles.container}>
+        <div
+          className={styles.arrowContainer}
+          style={{ left: 0 }}
+          onClick={() => handleArrow('l')}>
+          <Image
+            src='/img/arrowl.png'
+            alt=''
+            layout='fill'
+            objectFit='contain'
+          />
+        </div>
+        <div
+          className={styles.wrapper}
+          style={{
+            transform: `translateX(${-100 * index}vw)`,
+          }}>
+          {images.map((img, i) => (
+            <div
+              className={styles.imgContainer}
+              key={i}
+              style={{
+                width: '100%',
+                height: '500px',
+                alignItems: 'center',
+                marginTop: '30px',
+              }}>
+              <Image src={img} alt='' layout='fill' objectFit='contain' />
+            </div>
+          ))}
+        </div>
+        <div
+          className={styles.arrowContainer}
+          style={{ right: 0 }}
+          onClick={() => handleArrow('r')}>
+          <Image
+            src='/img/arrowr.png'
+            layout='fill'
+            alt=''
+            objectFit='contain'
+          />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Featured;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,21 +5,6 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <Image src='/img/logo.png' alt='' width='160px' height='69px' />
-      </div>
-      <div className={styles.item}>
-        <ul className={styles.list}>
-          <li className={styles.listItem}>Homepage</li>
-          <li className={styles.listItem}>Products</li>
-          <li className={styles.listItem}>Menu</li>
-
-          <li className={styles.listItem}>Events</li>
-          <li className={styles.listItem}>Blog</li>
-          <li className={styles.listItem}>Contact</li>
-        </ul>
-      </div>
-
-      <div className={styles.item}>
         <div className={styles.callButton}>
           <Image src='/img/telephone.png' alt='' width='32' height='32' />
         </div>
@@ -27,6 +12,17 @@ const Navbar = () => {
           <div className={styles.text}>ORDER NOW!</div>
           <div className={styles.text}>012 345 678</div>
         </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Products</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src='/img/logo.png' alt='' width='160px' height='69px' />
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contact</li>
+        </ul>
       </div>
       <div className={styles.item}>
         <div className={styles.cart}>
